838a13de16f7a71245cac077c18410cb782e54f1
mark424e <markphillip1800@gmail.com>
2026-08-20 14:43:55 +0200

    feat(data): add mock review data and pass to Trustpilot section

--- app/page.js
+++ app/page.js
@@ -47,13 +47,58 @@ const products = [
   },
 ]
 
+const reviews = [
+  {
+    id: 1,
+    name: 'John Doe',
+    title: 'Fantastisk!',
+    rating: 5,
+    comment: 'Fantastisk service og hurtig levering! Jeg er meget tilfreds med mit køb. Jeg vil helt sikkert anbefale Topdata til andre. Tak for en god oplevelse!',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    title: 'Meget tilfreds',
+    rating: 5,
+    comment: 'Kvaliteten af computeren er imponerende. Jeg har haft den i et par uger nu, og den kører problemfrit. Jeg er meget tilfreds med mit køb. Jeg vil helt sikkert anbefale Topdata til andre.',
+  },
+  {
+    id: 3,
+    name: 'Michael Johnson',
+    title: 'Det bedste køb ever!',
+    rating: 5,
+    comment: 'Jeg er meget tilfreds med mit køb! Topdata leverede en fantastisk oplevelse fra start til slut.',
+  },
+  {
+    id: 4,
+    name: 'Sarah Williams',
+    title: 'Glad kunde for anden gang :)',
+    rating: 5,
+    comment: 'For ti år siden købte jeg en high-end PC hos Topdata, og den kører stadig upåklageligt. Jeg ville dog gerne have en mere moderne model, så jeg gik igen til Topdata. Jeg oplevede en lille udfordring med den nye maskine, men det blev fikset lynhurtigt og professionelt. Det bliver 5 stjerner herfra pga venlig betjening og god vejledning inden køb. Om ti år går jeg gerne til Topdata igen.',
+  },
+  {
+    id: 5,
+    name: 'David Brown',
+    title: 'Rigtig god kvalitet',
+    rating: 5,
+    comment: 'Har solgt min gamle gamer computer, og udskiftet med en ny fra Topdata. Den ser mega fed ud og spiller bare maks. Er meget glad :)',
+  },
+  {
+    id: 6,
+    name: 'Lisa Davis',
+    title: 'Kan godt anbefale',
+    rating: 5,
+    comment: 'God og venlig service når man ringer til dem med spørgsmål :D Levering til tiden. Gode priser :)',
+  }
+]
+
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
       <Selection products={products} />
-      <Trustpilot />
+      <Trustpilot reviews={reviews} />
       <Footer />
     </main>
   );
