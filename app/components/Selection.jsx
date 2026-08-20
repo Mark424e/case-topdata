b4b304271daa607b4014a930698876b2c0defc41
mark424e <markphillip1800@gmail.com>
2026-08-20 14:44:09 +0200

    style(selection): add section heading to product grid

--- app/components/Selection.jsx
+++ app/components/Selection.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import ProductCard from './ProductCard'
+import ProductCard from "./ProductCard";
 
 const Selection = ({ products }) => {
   return (
     <div>
       <div className="bg-primary/5">
         <div className="container mx-auto">
-          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-30">
-            {products.map((product) => (
-
-              <ProductCard
-                key={product.id}
-                title={product.title}
-                image={product.image}
-                category={product.category}
-                specs={product.specs}
-                currentPrice={product.currentPrice}
-                originalPrice={product.originalPrice}
-                discount={product.discount}
-              />
-            ))}
+          <div className="py-30 text-center">
+            <h2 className="text-5xl mb-12">Udvalgte maskiner fra Top Prime-serien</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+              {products.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  title={product.title}
+                  image={product.image}
+                  category={product.category}
+                  specs={product.specs}
+                  currentPrice={product.currentPrice}
+                  originalPrice={product.originalPrice}
+                  discount={product.discount}
+                />
+              ))}
+          </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Selection
+export default Selection;
