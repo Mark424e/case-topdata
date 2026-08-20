c7a2d3e9efe5cfc127922d7764926e283d048451
mark424e <markphillip1800@gmail.com>
2026-08-20 13:32:51 +0200

    feat(products): add background to component

--- app/components/Selection.jsx
+++ app/components/Selection.jsx
@@ -4,20 +4,26 @@ import ProductCard from './ProductCard'
 
 const Selection = ({ products }) => {
   return (
-    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {products.map((product) => (
+    <div>
+      <div className="bg-primary/5">
+        <div className="container mx-auto">
+          <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-30">
+            {products.map((product) => (
 
-        <ProductCard
-          key={product.id}
-          title={product.title}
-          image={product.image}
-          category={product.category}
-          specs={product.specs}
-          currentPrice={product.currentPrice}
-          originalPrice={product.originalPrice}
-          discount={product.discount}
-        />
-      ))}
+              <ProductCard
+                key={product.id}
+                title={product.title}
+                image={product.image}
+                category={product.category}
+                specs={product.specs}
+                currentPrice={product.currentPrice}
+                originalPrice={product.originalPrice}
+                discount={product.discount}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
